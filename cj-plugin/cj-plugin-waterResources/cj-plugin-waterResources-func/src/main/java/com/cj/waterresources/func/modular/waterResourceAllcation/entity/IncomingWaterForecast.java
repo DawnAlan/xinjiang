4a9de41d693dd82037c728e8cb226f08c7b063f2
Atlantic Ocean <1083570642@qq.com>
2023-12-20 14:29:39 +0800

    水资源调配业务系统

---
+++ cj-plugin/cj-plugin-waterResources/cj-plugin-waterResources-func/src/main/java/com/cj/waterresources/func/modular/waterResourceAllcation/entity/IncomingWaterForecast.java
@@ -0,0 +1,89 @@
+package com.cj.waterresources.func.modular.waterResourceAllcation.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 来水预报
+ * @TableName INCOMING_WATER_FORECAST
+ */
+@TableName(value ="INCOMING_WATER_FORECAST")
+@Data
+public class IncomingWaterForecast implements Serializable {
+    /**
+     * 主键id
+     */
+    @TableId(value = "ID")
+    private String id;
+
+    /**
+     * 方案名称
+     */
+    @TableField(value = "PROGRAMME_NAME")
+    private String programmeName;
+
+    /**
+     * 模型类型(1-中长期 2-短期 3-场次)
+     */
+    @TableField(value = "MODEL_TYPE")
+    private Integer modelType;
+
+    /**
+     * 预报时间
+     */
+    @TableField(value = "PREDICTION_TIME")
+    private Date predictionTime;
+
+    /**
+     * 时段类型(1-月 2-旬 3-日 4-小时)
+     */
+    @TableField(value = "PERIOD_TIME_TYPE")
+    private Integer periodTimeType;
+
+    /**
+     * 时段步长
+     */
+    @TableField(value = "PERIOD_TIME_STEP")
+    private Integer periodTimeStep;
+
+    /**
+     * 时间数量
+     */
+    @TableField(value = "PERIOD_TIME_NUM")
+    private Integer periodTimeNum;
+
+    /**
+     * 备注
+     */
+    @TableField(value = "REMARK")
+    private String remark;
+
+    /**
+     * 创建时间
+     */
+    @TableField(value = "CREATE_TIME")
+    private Date createTime;
+
+    /**
+     * 创建人
+     */
+    @TableField(value = "CREATE_BY")
+    private String createBy;
+
+    @TableField(value = "MODEL_RESULT_ADDRESS")
+    private String modelResultAddress;
+
+    /**
+     * 结束时间
+     */
+    @TableField(value = "END_TIME")
+    private Date endTime;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
