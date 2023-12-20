7691a14970f586ad8dcbbbc392e2bf7678ca1500
Atlantic Ocean <1083570642@qq.com>
2023-12-20 14:23:42 +0800

    水资源调配模型

--- cj-plugin/cj-plugin-model/cj-plugin-model-func/src/main/java/com/cj/model/func/modular/watertransfer/entity/Waterdemand.java
+++ cj-plugin/cj-plugin-model/cj-plugin-model-func/src/main/java/com/cj/model/func/modular/watertransfer/entity/Waterdemand.java
@@ -2,12 +2,23 @@ package com.cj.model.func.modular.watertransfer.entity;
 
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 public class Waterdemand {
-    private Date time;
-    private double preQ;
-    private int scale;
-    private String location;
+
+    //用水计划类型
+    private String useWaterPlan;
+    //用水数据
+    private double waterDemendData;
+    //地区
+    private String area;
+    //单位
+    private String unit;
+    //日计划填写
+    private String subArea;
+    //表头
+    private String colName;
+    //子节点
+    private int children;
+
+
 }
