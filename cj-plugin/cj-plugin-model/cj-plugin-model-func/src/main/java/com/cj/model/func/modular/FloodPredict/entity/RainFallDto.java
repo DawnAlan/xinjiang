b25ae32331669184772dab01522992e5c0194f21
leoluoxu <leoluoxu@com>
2023-12-29 18:01:43 +0800

    add

---
+++ cj-plugin/cj-plugin-model/cj-plugin-model-func/src/main/java/com/cj/model/func/modular/FloodPredict/entity/RainFallDto.java
@@ -0,0 +1,17 @@
+package com.cj.model.func.modular.FloodPredict.entity;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class RainFallDto {
+
+    @ApiModelProperty(value = "时间")
+    private String date;
+
+    @ApiModelProperty(value = "降雨量")
+    private Double rainFall;
+
+    @ApiModelProperty(value = "降雨地区")
+    private String area;
+}
