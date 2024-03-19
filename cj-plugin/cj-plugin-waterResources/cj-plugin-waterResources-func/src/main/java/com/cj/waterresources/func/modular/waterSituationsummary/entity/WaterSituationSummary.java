b0bf80da0dee1bcbe714c3d4604a43792141f224
leoluoxu <leoluoxu@com>
2024-03-19 17:32:45 +0800

    update

--- cj-plugin/cj-plugin-waterResources/cj-plugin-waterResources-func/src/main/java/com/cj/waterresources/func/modular/waterSituationsummary/entity/WaterSituationSummary.java
+++ cj-plugin/cj-plugin-waterResources/cj-plugin-waterResources-func/src/main/java/com/cj/waterresources/func/modular/waterSituationsummary/entity/WaterSituationSummary.java
@@ -2,6 +2,8 @@ package com.cj.waterresources.func.modular.waterSituationsummary.entity;
 
 import java.math.BigDecimal;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
@@ -10,7 +12,7 @@ import java.io.Serializable;
  * @author makejava
  * @since 2024-03-19 17:25:30
  */
-@SuppressWarnings("serial")
+@Data
 public class WaterSituationSummary extends Model<WaterSituationSummary> {
     //主键id
     private Integer id;
@@ -27,81 +29,7 @@ public class WaterSituationSummary extends Model<WaterSituationSummary> {
     //日
     private Integer day;
     //值
-    private BigDecimal value;
+    private Double value;
 
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getReservoirName() {
-        return reservoirName;
-    }
-
-    public void setReservoirName(String reservoirName) {
-        this.reservoirName = reservoirName;
-    }
-
-    public String getSiteName() {
-        return siteName;
-    }
-
-    public void setSiteName(String siteName) {
-        this.siteName = siteName;
-    }
-
-    public String getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(String dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public Integer getMonth() {
-        return month;
-    }
-
-    public void setMonth(Integer month) {
-        this.month = month;
-    }
-
-    public Integer getDay() {
-        return day;
-    }
-
-    public void setDay(Integer day) {
-        this.day = day;
-    }
-
-    public BigDecimal getValue() {
-        return value;
-    }
-
-    public void setValue(BigDecimal value) {
-        this.value = value;
-    }
-
-    /**
-     * 获取主键值
-     *
-     * @return 主键值
-     */
-    @Override
-    protected Serializable pkVal() {
-        return this.id;
-    }
-    }
+}
 
