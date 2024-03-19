27f3e6dced2f305bae195bead46ce3b19d28f0d0
Atlantic Ocean <1083570642@qq.com>
2024-03-19 18:02:37 +0800

    防洪首页水库预览用定时任务计算存入redis,flood.schedule.enabled 配置是否启用定时任务

---
+++ cj-plugin/cj-plugin-flood/cj-plugin-flood-func/src/main/java/com/cj/flood/func/modular/homePage/scheduler/HomePageTask.java
@@ -0,0 +1,22 @@
+package com.cj.flood.func.modular.homePage.scheduler;
+
+
+import com.cj.flood.func.modular.homePage.service.FloodHomePageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+@ConditionalOnProperty(prefix = "flood.schedule", name = "enabled", havingValue = "true", matchIfMissing = true)
+public class HomePageTask {
+    private final FloodHomePageService floodHomePageService;
+
+    @Scheduled(cron = "0 */1 0/1 * * ? ")
+    public void waterFlowAlarmEventPolling() {
+        floodHomePageService.waterStorageOverviewSchedule(new Date());
+    }
+}
