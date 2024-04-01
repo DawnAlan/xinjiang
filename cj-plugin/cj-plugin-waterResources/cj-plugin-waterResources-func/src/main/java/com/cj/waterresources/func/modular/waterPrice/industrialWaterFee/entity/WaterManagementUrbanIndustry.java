7863f1423100afbf5acd22e61b7cbc04501e4a36
leoluoxu <leoluoxu@com>
2024-04-01 10:55:01 +0800

    修改

--- cj-plugin/cj-plugin-waterResources/cj-plugin-waterResources-func/src/main/java/com/cj/waterresources/func/modular/waterPrice/industrialWaterFee/entity/WaterManagementUrbanIndustry.java
+++ cj-plugin/cj-plugin-waterResources/cj-plugin-waterResources-func/src/main/java/com/cj/waterresources/func/modular/waterPrice/industrialWaterFee/entity/WaterManagementUrbanIndustry.java
@@ -1,6 +1,8 @@
 package com.cj.waterresources.func.modular.waterPrice.industrialWaterFee.entity;
 
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
@@ -9,7 +11,7 @@ import java.io.Serializable;
  * @author makejava
  * @since 2024-04-01 10:52:03
  */
-@SuppressWarnings("serial")
+@Data
 public class WaterManagementUrbanIndustry extends Model<WaterManagementUrbanIndustry> {
     //站点编号
     private String siteCode;
@@ -58,199 +60,5 @@ public class WaterManagementUrbanIndustry extends Model<WaterManagementUrbanIndu
     //ID
     private String id;
 
-
-    public String getSiteCode() {
-        return siteCode;
-    }
-
-    public void setSiteCode(String siteCode) {
-        this.siteCode = siteCode;
-    }
-
-    public String getSiteName() {
-        return siteName;
-    }
-
-    public void setSiteName(String siteName) {
-        this.siteName = siteName;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public Integer getMonth() {
-        return month;
-    }
-
-    public void setMonth(Integer month) {
-        this.month = month;
-    }
-
-    public Double getAgriculturalWaterPrice() {
-        return agriculturalWaterPrice;
-    }
-
-    public void setAgriculturalWaterPrice(Double agriculturalWaterPrice) {
-        this.agriculturalWaterPrice = agriculturalWaterPrice;
-    }
-
-    public Double getIndustrialWaterPrice() {
-        return industrialWaterPrice;
-    }
-
-    public void setIndustrialWaterPrice(Double industrialWaterPrice) {
-        this.industrialWaterPrice = industrialWaterPrice;
-    }
-
-    public Double getWaterResourceTaxes() {
-        return waterResourceTaxes;
-    }
-
-    public void setWaterResourceTaxes(Double waterResourceTaxes) {
-        this.waterResourceTaxes = waterResourceTaxes;
-    }
-
-    public Integer getAgriculturalProportion() {
-        return agriculturalProportion;
-    }
-
-    public void setAgriculturalProportion(Integer agriculturalProportion) {
-        this.agriculturalProportion = agriculturalProportion;
-    }
-
-    public Integer getIndustrialProportion() {
-        return industrialProportion;
-    }
-
-    public void setIndustrialProportion(Integer industrialProportion) {
-        this.industrialProportion = industrialProportion;
-    }
-
-    public Double getTotalWaterFeesPayable() {
-        return totalWaterFeesPayable;
-    }
-
-    public void setTotalWaterFeesPayable(Double totalWaterFeesPayable) {
-        this.totalWaterFeesPayable = totalWaterFeesPayable;
-    }
-
-    public Double getTotalPaidWaterFees() {
-        return totalPaidWaterFees;
-    }
-
-    public void setTotalPaidWaterFees(Double totalPaidWaterFees) {
-        this.totalPaidWaterFees = totalPaidWaterFees;
-    }
-
-    public Double getTotalSurplusWaterFees() {
-        return totalSurplusWaterFees;
-    }
-
-    public void setTotalSurplusWaterFees(Double totalSurplusWaterFees) {
-        this.totalSurplusWaterFees = totalSurplusWaterFees;
-    }
-
-    public Double getAgriculturalWaterFeesPayable() {
-        return agriculturalWaterFeesPayable;
-    }
-
-    public void setAgriculturalWaterFeesPayable(Double agriculturalWaterFeesPayable) {
-        this.agriculturalWaterFeesPayable = agriculturalWaterFeesPayable;
-    }
-
-    public Double getAgriculturalPaidWaterFees() {
-        return agriculturalPaidWaterFees;
-    }
-
-    public void setAgriculturalPaidWaterFees(Double agriculturalPaidWaterFees) {
-        this.agriculturalPaidWaterFees = agriculturalPaidWaterFees;
-    }
-
-    public Double getAgriculturalSurplusWaterFees() {
-        return agriculturalSurplusWaterFees;
-    }
-
-    public void setAgriculturalSurplusWaterFees(Double agriculturalSurplusWaterFees) {
-        this.agriculturalSurplusWaterFees = agriculturalSurplusWaterFees;
-    }
-
-    public Double getIndustrialWaterFeesPayable() {
-        return industrialWaterFeesPayable;
-    }
-
-    public void setIndustrialWaterFeesPayable(Double industrialWaterFeesPayable) {
-        this.industrialWaterFeesPayable = industrialWaterFeesPayable;
-    }
-
-    public Double getIndustrialPaidWaterFees() {
-        return industrialPaidWaterFees;
-    }
-
-    public void setIndustrialPaidWaterFees(Double industrialPaidWaterFees) {
-        this.industrialPaidWaterFees = industrialPaidWaterFees;
-    }
-
-    public Double getIndustrialSurplusWaterFees() {
-        return industrialSurplusWaterFees;
-    }
-
-    public void setIndustrialSurplusWaterFees(Double industrialSurplusWaterFees) {
-        this.industrialSurplusWaterFees = industrialSurplusWaterFees;
-    }
-
-    public Double getWaterResourceWaterFeesPayable() {
-        return waterResourceWaterFeesPayable;
-    }
-
-    public void setWaterResourceWaterFeesPayable(Double waterResourceWaterFeesPayable) {
-        this.waterResourceWaterFeesPayable = waterResourceWaterFeesPayable;
-    }
-
-    public Double getWaterResourcePaidWaterFees() {
-        return waterResourcePaidWaterFees;
-    }
-
-    public void setWaterResourcePaidWaterFees(Double waterResourcePaidWaterFees) {
-        this.waterResourcePaidWaterFees = waterResourcePaidWaterFees;
-    }
-
-    public Double getWaterResourceSurplusWaterFees() {
-        return waterResourceSurplusWaterFees;
-    }
-
-    public void setWaterResourceSurplusWaterFees(Double waterResourceSurplusWaterFees) {
-        this.waterResourceSurplusWaterFees = waterResourceSurplusWaterFees;
-    }
-
-    public Double getAnnualCumulativeWaterConsumption() {
-        return annualCumulativeWaterConsumption;
-    }
-
-    public void setAnnualCumulativeWaterConsumption(Double annualCumulativeWaterConsumption) {
-        this.annualCumulativeWaterConsumption = annualCumulativeWaterConsumption;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    /**
-     * 获取主键值
-     *
-     * @return 主键值
-     */
-    @Override
-    protected Serializable pkVal() {
-        return this.siteCode;
-    }
-    }
+}
 
