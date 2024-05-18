44a0d7f82106231972a27ab52d6614a851abe47e
Atlantic Ocean <1083570642@qq.com>
2024-05-18 19:25:22 +0800

    double/list<double>保留两/三位小数序列化类

---
+++ cj-base/cj-common/src/main/java/com/cj/common/serializer/DoubleListScale2Serializer.java
@@ -0,0 +1,25 @@
+package com.cj.common.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class DoubleListScale2Serializer extends JsonSerializer<List<Double>> {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void serialize(List<Double> value, JsonGenerator gen, SerializerProvider provider)
+            throws IOException {
+        if (value != null) {
+            double[] newValue = new double[value.size()];
+            for (int i = 0; i < value.size(); i++) {
+                newValue[i] = new BigDecimal(value.get(i).toString()).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+            }
+            gen.writeArray(newValue, 0, newValue.length);
+        }
+    }
+}
---
+++ cj-base/cj-common/src/main/java/com/cj/common/serializer/DoubleScale3Serializer.java
@@ -0,0 +1,21 @@
+package com.cj.common.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class DoubleScale3Serializer extends JsonSerializer<Double> {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void serialize(Double value, JsonGenerator gen, SerializerProvider provider)
+            throws IOException {
+        if (value != null) {
+            BigDecimal bigDecimal = new BigDecimal(value.toString()).setScale(3, BigDecimal.ROUND_HALF_UP);
+            gen.writeNumber(bigDecimal);
+        }
+    }
+}
