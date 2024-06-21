a4ef72a8f79a44300361609a7e4613195a1b50e8
Atlantic Ocean <1083570642@qq.com>
2024-06-21 19:34:39 +0800

    消费端feign接口放到common模块

--- cj-base/cj-common/src/main/java/com/cj/common/feign/WaterSituationClient.java
+++ cj-base/cj-common/src/main/java/com/cj/common/feign/WaterSituationClient.java
@@ -1,4 +1,4 @@
-package com.cj.flood.func.core.common.feign;
+package com.cj.common.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
